d69729508d18d8245b824c577ee2e19b6ffa46de
shekharpareek <shekhar.pareek@sunarctechnologies.com>
2023-03-03 17:05:17 +0530

    updated committed

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.defaults({ ease: "none", duration: 2 });
 
-var timeline = gsap.timeline({});
-
-timeline.to(".wrapper", {
-  duration: 60,
-  transform: "scale(3.5)",
+var timeline = gsap.timeline({
   scrollTrigger: {
     trigger: ".wrapper",
     start: "top ",
+    // end: "200%",
     end: "bottom",
-    scrub: 3,
+    scrub: 1,
     duration: 4,
     ease: "slow",
-    toggleActions: "restart none none none",
-    pin: true,
+    // toggleActions: "restart none none none",
+    pin: ".container",
   },
+});
+timeline.to(".wrapper", {
+  duration: 90,
+  transform: "scale(3.5)",
 });
